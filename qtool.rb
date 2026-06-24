15fe416f28714b7cdab75e4e51f172832dd1d1e4
dvlproad <dvlproad@163.com>
2026-06-25 05:47:01 +0800

    qtool 0.9.36: JSON 不嵌入 .app，存绝对路径

--- qtool.rb
+++ qtool.rb
@@ -2,11 +2,11 @@ class Qtool < Formula
   desc "QTool:含分支json文件的创建+Jenkins打包"
   homepage "https://github.com/dvlpCI/script-branch-json-file"
 
-  version "0.9.35"
+  version "0.9.36"
   # revision 1  # 版本号不变，但 revision 递增，会触发升级
   url "https://github.com/dvlpCI/script-branch-json-file/archive/#{version}.tar.gz"
   # url "https://github.com/dvlpCI/script-branch-json-file/archive/0.9.13.tar.gz"
-  sha256 "8b0e840b65f2f0e202913a88727ef678c66bf141841be525a0fc6054cab750c7"
+  sha256 "408afd58af81a93232a75f11dd1033946de8050d2277d52e02f479c50898a02a"
   # shasum -a 256 xxxx.tar.gz
   # version /(\d+\.\d+\.\d+)/  # 从 URL 中提取版本号
 
